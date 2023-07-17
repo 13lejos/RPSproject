c4da453fd91742806fd6eb1e7b8c97f4d9c6a6b6
Joseph Le <13lejoseph@gmail.com>
2023-07-16 21:45:55 -0700

    initial commit RPS game

---
+++ script.js
@@ -0,0 +1,56 @@
+
+//pick a number between 1-3
+//assign rock, paper, sicssor to a number
+//have the computer randomly pick a number
+//rock beats sicssor, paper beats rock, scissor beats paper
+
+
+// different functions that I don't want in the index yet
+
+function pickRPS(string)
+{
+const rock = 1
+const paper = 2
+const scissor = 3
+
+function getRandom()
+{
+   const min = 1
+   const max = 4 
+   let computerSelection = ( Math.random() * (max-min) + min )
+}
+
+return function getRandom
+}
+
+
+let sign = prompt("Rock paper or scissors?");    
+let yourSelect
+
+if (sign.toLowerCase() === "rock") 
+{
+alert("you chose rock!");
+yourSelect = 1
+
+
+}
+else if (sign.toLowerCase() === "paper") 
+{
+alert("you chose paper!");
+yourSelect = 2
+}
+else if (sign.toLowerCase() === "scissors") 
+{
+alert("you chose scissors!");
+yourSelect = 3
+}
+
+
+function getRandom(){
+
+    let computerSelection 
+    const min = Math.ceil(1)
+    const max = Math.floor(4)
+   computerSelection = Math.floor(Math.random() * (max-min) + min )
+    console.log(computerSelection)
+}
