a90a1f36458ae4dd99eddb7d08ab276c96403cc0
Joseph Le <13lejoseph@gmail.com>
2023-07-19 22:49:26 -0700

    add print functions gamecounter

---
+++ chopshop.js
@@ -0,0 +1,9 @@
+// this is an empty code workspace 
+
+document.getElementById('logReport').createElement('p');
+
+
+function printScore(){
+
+document.appendChild(yourScore)
+}
--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //declares starting score
 let yourScore = 0
 let computerScore = 0
+let scoring
+let yourPick
+let computerSelection 
+let gameCounter = 1
 
 document.getElementById('yours').innerHTML = yourScore;
 document.getElementById('theirs').innerHTML = computerScore;
@@ -9,19 +13,20 @@ document.getElementById('theirs').innerHTML = computerScore;
 // function calls a game of RPS
 function playRound(yourSelect){
 
+    
 // Declare your choice
 // Store your choice as a number
 // call a choice from the computer
 //   
 
 let computerSelect = getRandom();
-let scoring
+
 
 // call function to have the computer select a number 1-3
 // then assign it a string name 
 function getRandom()
 {
-let computerSelection 
+
 const min = Math.ceil(1)
 const max = Math.floor(4)
 computerSelection = Math.floor(Math.random() * (max-min) + min )
@@ -30,16 +35,16 @@ return computerSelection;
 }
 
 if (computerSelect === 1){
-    computerSelect = "rock"
+    computerSelection = "rock"
 }   
 else if (computerSelect === 2){
-    computerSelect = "paper"
+    computerSelection = "paper"
 }   
 else if (computerSelect === 3){
-    computerSelect = "scissors"
+    computerSelection = "scissors"
 }   
 
-// determine your choice and compare it to the computer's choice
+/* determine your choice and compare it to the computer's choice
 if (yourSelect === 1) 
 {
 alert("you chose rock! " + "the computer chose " + computerSelect + ".");
@@ -107,12 +112,85 @@ else if(yourSelect === 3 && computerSelect === "paper")
     yourScore = ++yourScore
     alert("You " + scoring + "! your score is " + yourScore + " the computer's score is " + computerScore)
 }
+*/
+if (yourSelect === 1 && computerSelection === "rock")
+{
+    scoring = "tied"
+    yourPick = "rock"
+    printScore()
+    
+}
+else if (yourSelect === 1 && computerSelection === "paper")
+{
+    scoring = "lost"
+    computerScore = ++computerScore
+    yourPick = "rock"
+    printScore()
+    
+}
+else if(yourSelect === 1 && computerSelection === "scissors")
+{
+    scoring = "won"
+    yourScore = ++yourScore
+    yourPick = "rock"
+    printScore()
+    
+}
+
+else if (yourSelect === 2 && computerSelection === "paper")
+{
+    scoring = "tied"
+    yourPick = "paper"
+    printScore()
+   
+}
+
+else if (yourSelect === 2 && computerSelection === "scissors")
+{
+    scoring = "lost"
+    computerScore = ++computerScore
+    yourPick = "paper"
+    printScore()
+   
+}
+else if(yourSelect === 2 && computerSelection === "rock")
+{
+    scoring = "won"
+    yourScore = ++yourScore
+    yourPick = "paper"
+    printScore()
+    
+}
+else if(yourSelect === 3 && computerSelection === "scissors")
+{
+    scoring = "tied"
+    yourPick = "scissors"
+    printScore()
+    
+}
+else if (yourSelect === 3 && computerSelection === "rock")
+{
+    scoring = "lost"
+    computerScore = ++computerScore
+    yourPick = "scissors"
+    printScore()
+   
+}
+else if(yourSelect === 3 && computerSelection === "paper")
+{
+    scoring = "won"
+    yourScore = ++yourScore
+    yourPick = "scissors"
+    printScore()
+    
+}
+
 
 
 
 document.getElementById('yours').innerHTML = yourScore;
 document.getElementById('theirs').innerHTML = computerScore;
-
+gameCounter = ++gameCounter
 }
 
 function resetGame(){
@@ -121,4 +199,15 @@ computerScore = 0
 
 document.getElementById('yours').innerHTML = yourScore;
 document.getElementById('theirs').innerHTML = computerScore;
+
+
+}
+
+function printScore()
+{
+    
+    const logReport = document.getElementById("logReport")
+    let reported = document.createElement('log')
+    logReport.appendChild(reported)
+    reported.textContent = ("Game "+ gameCounter + ": " + "You picked " + yourPick + "! The computer picked " + computerSelection + " you " + scoring + "!");
 }
